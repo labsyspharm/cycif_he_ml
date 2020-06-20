778a19eac12f6aa3209bab8c440bd3c161b3cbbb
Hung-Yi Wu <hungyiwu@protonmail.com>
2020-06-20 11:30:43 -0400

    subsample for faster visual inspection

--- code/viz_quantify.py
+++ code/viz_quantify.py
@@ -1,7 +1,17 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 
-df = pd.read_csv('../data/aligned_feature.csv')
-plt.scatter(df['DNA6'], df['CD20'])
-plt.show()
+if __name__ == '__main__':
+    df = pd.read_csv('../data/aligned_feature.csv')
+    X = df.sample(n=int(1e4))\
+            [['DNA6', 'CD20']].values
+    d = stats.gaussian_kde(X.T)(X.T)
+    sk = np.argsort(d)
+    X, d = X[sk, :], d[sk]
+
+    plt.scatter(X[:, 0], X[:, 1], c=d, cmap='coolwarm', s=1)
+    plt.xlabel('DNA6')
+    plt.ylabel('CD20')
+    plt.show()
