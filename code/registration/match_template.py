94f4fb8dda1e598277a56cc7730a0971c0eeb57e
Hung-Yi Wu <hungyiwu@protonmail.com>
2020-06-25 13:51:46 -0400

    change boundary handling

--- code/registration/match_template.py
+++ code/registration/match_template.py
@@ -4,10 +4,12 @@ import yaml
 
 import numpy as np
 import h5py
+import dask
 import dask.array as da
 
 from skimage.morphology import disk
-from skimage.feature import match_template
+#from skimage.feature import match_template
+from template import match_template
 
 if __name__ == '__main__':
     # parse config
@@ -17,31 +19,33 @@ if __name__ == '__main__':
 
     in1_filepath = config['in1_filepath']
     in1_name = config['in1_name']
+    in1_chunk_size = config['in1_chunk_size']
     in2_filepath = config['in2_filepath']
     in2_name = config['in2_name']
+    in2_chunk_size = config['in2_chunk_size']
     out_filepath = config['out_filepath']
-    chunk_size = config['chunk_size']
     overwrite = config['overwrite']
 
     # load data
     f1 = h5py.File(in1_filepath, 'r')[in1_name]
     f2 = h5py.File(in2_filepath, 'r')[in2_name]
-    arr1 = da.from_array(f1, chunks=chunk_size)
-    arr2 = da.from_array(f2, chunks=chunk_size)
+    arr1 = da.from_array(f1, chunks=in1_chunk_size)
+    arr2 = da.from_array(f2, chunks=in2_chunk_size)
 
     # define match function
-    selem = arr1[0:chunk_size[0], 0:chunk_size[1]]
-    match_fn = lambda block: match_template(
-            image=block,
+    selem = arr1[0:in1_chunk_size[0], 0:in1_chunk_size[1]]
+    match_fn = lambda b: match_template(
+            image=b,
             template=selem,
-            pad_input=False,
+            pad_input=True,
             )
 
     # map function across overlapping chunks
     overlap_depth = {0: selem.shape[0]//2, 1: selem.shape[1]//2}
     boundary = {0: 0, 1: 0}
-    res = arr2.map_overlap(match_fn, depth=overlap_depth, boundary=boundary,
-            dtype=np.float64, trim=False, chunks=arr2.chunks)
+    res = arr2.map_overlap(match_fn,
+            depth=overlap_depth, boundary=boundary,
+            dtype=float, trim=True)
 
     # save to disk
     if overwrite and os.path.isfile(out_filepath):
