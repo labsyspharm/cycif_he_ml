9c6f9f229bcf884e07269ebc72f851acc8e675f7
Hung-Yi Wu <hungyiwu@protonmail.com>
2020-07-01 12:30:15 -0400

    add doc string and python-fire

---
+++ code/se_postprocessing.py
@@ -0,0 +1,57 @@
+import numpy as np
+import fire
+
+from skimage import io
+
+def se_postprocessing(in_filepath: str, out_filepath: str, dtype: str):
+    '''
+    Post-processing for SimpleElastix output: dtype casting and range clipping.
+    SimpleElastix output has roughly same value range (may exceed range of
+    input data type due to interpolation) but cased to float32.
+
+    Args:
+        in_filepath, out_filepath: str
+            Paths to input and output images
+        dtype: str
+            Image data type conversion. Accepted formats are bool, uint8, int16,
+            uint16, float32, float64.
+    '''
+    # check dtype argument
+    if dtype == 'bool':
+        cast_fn = util.img_as_bool
+    elif dtype == 'uint8':
+        cast_fn = util.img_as_ubyte
+    elif dtype == 'int16':
+        cast_fn = util.img_as_int
+    elif dtype == 'uint16':
+        cast_fn = util.img_as_uint
+    elif dtype == 'float32':
+        cast_fn = util.img_as_float32
+    elif dtype == 'float64':
+        cast_fn = util.img_as_float64
+    else:
+        raise NotImplementedError('dtype {} not recognized. Currently accepted'
+                'formats are bool, uint8, int16, uint16, float32, float64.'
+                .format(dtype))
+
+    # load image
+    img = io.imread(in_filepath)
+
+    # clip
+    if 'float' in dtype:
+        info = np.finfo(dtype)
+        a_max, a_min = info.max, info.min
+    elif 'int' in dtype:
+        info = np.iinfo(dtype)
+        a_max, a_min = info.max, info.min
+    else:
+        a_max, a_min = True, False
+
+    img = np.clip(img, a_min=a_min, a_max=a_max).astype(dtype)
+
+    # save to disk
+    io.imsave(out_filepath, img)
+
+
+if __name__ == '__main__':
+    fire.Fire(se_postprocessing)
---
+++ code/se_run.py
@@ -0,0 +1,47 @@
+import SimpleITK as sitk
+import fire
+
+
+def se_run(fixed_filepath: str, moving_filepath: str, out_filepath: str,
+        nres: int=4, niter: int=256):
+    '''
+    Register one image to another using intensity-based affine transformation
+    with SimpleElastix library (C++ library with Python interface).
+
+    Args:
+        fixed_filepath, moving_filepath, out_filepath: str
+            Paths of fixed, moving, and registered moving images.
+        nres: int [optional]
+            Number of resolutions. SimpleElastix performs multi-resolution
+            algorithms; more resolution allows fewer iterations but more
+            computer memory. Default is 4 layers.
+        niter: int [optional]
+            Maximum number of iterations. Default is 256.
+    '''
+    # load data
+    fixed = sitk.ReadImage(fixed_filepath)
+    moving = sitk.ReadImage(moving_filepath)
+
+    # configure image filter
+    img_filter = sitk.ElastixImageFilter()
+    img_filter.SetFixedImage(fixed)
+    img_filter.SetMovingImage(moving)
+
+    # configure transformation
+    pm = sitk.GetDefaultParameterMap('affine')
+    pm['NumberOfResolutions'] = [str(nres)]
+    pm['MaximumNumberOfIterations'] = [str(niter)]
+    vec = sitk.VectorOfParameterMap()
+    vec.append(pm)
+    img_filter.SetParameterMap(vec)
+
+    # execute registration
+    img_filter.Execute()
+    out = img_filter.GetResultImage()
+
+    # save output to disk
+    sitk.WriteImage(out, out_filepath)
+
+
+if __name__ == '__main__':
+    fire.Fire(se_run)
