c315bea3db50997560f6ea582ca2a57227c257e7
Hung-Yi Wu <hywu0110@gmail.com>
2020-07-14 20:47:33 -0400

    split step 2

--- shell_scripts/step_2b.sh
+++ shell_scripts/step_2b.sh
@@ -6,16 +6,6 @@ CONFIG_FILEPATH="$1"
 source $CONFIG_FILEPATH
 
 
-echo "run registration"
-python "$CODE_DIRPATH/se_fit.py"\
-    --fixed_filepath $CYCIF_TIFF_FILEPATH\
-    --moving_filepath $HE_TIFF_FILEPATH\
-    --out_filepath $TMP_TIFF_FILEPATH\
-    --nres $AFFINE_NRES\
-    --niter $AFFINE_NITER
-mv "./TransformParameters.0.txt" $PARAM_FILEPATH
-
-
 echo "post-processing registration preview"
 python "$CODE_DIRPATH/se_postrocessing.py"\
     --in_filepath $TMP_TIFF_FILEPATH\
