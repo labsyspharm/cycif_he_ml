5e100b8a0cb9b30a8dbe8fced9990b9e2311ab32
Hung-Yi Wu <16283808+hungyiwu@users.noreply.github.com>
2020-07-24 11:16:33 -0400

    add downscale for TNP, chunk size format fix (#24)
    Co-authored-by: Hung-Yi Wu <hywu0110@gmail.com>

--- shell_scripts/step_1a.sh
+++ shell_scripts/step_1a.sh
@@ -15,7 +15,15 @@ python "$CODE_DIRPATH/tiff2hdf5.py"\
     --overwrite "True"
 python "$CODE_DIRPATH/tiff2hdf5.py"\
     --in_filepath $HE_OMETIF_FILEPATH\
-    --out_filepath $HE_RGB_H5_FILEPATH\
+    --out_filepath $TMP_H5_FILEPATH\
     --chunk_size $CHUNKSIZE\
     --channel_names "r,g,b"\
+    --overwrite "True"
+
+echo "downscale H&E for TNP project"
+python "$CODE_DIRPATH/downscale.py"\
+    --in_filepath $TMP_H5_FILEPATH\
+    --out_filepath $HE_RGB_H5_FILEPATH\
+    --factors "(2,2)"\
+    --chunk_size $CHUNKSIZE\
     --overwrite "True"
