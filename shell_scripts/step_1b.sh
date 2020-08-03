1442b6b726d517f588bc35f9a4f752d8eab41855
Hung-Yi Wu <hywu0110@gmail.com>
2020-08-03 13:50:58 -0400

    add to shell script

--- shell_scripts/step_1b.sh
+++ shell_scripts/step_1b.sh
@@ -24,7 +24,14 @@ python "$CODE_DIRPATH/se_prep.py"\
     --overwrite "True"
 python "$CODE_DIRPATH/se_prep.py"\
     --in_filepath $HE_GRAY_H5_FILEPATH\
-    --out_filepath $HE_TIFF_FILEPATH\
+    --out_filepath $TMP_TIFF_FILEPATH\
     --channel_names "[gray,]"\
     --flip "True"\
+    --overwrite "True"
+
+echo "transform by anchor"
+python "$CODE_DIRPATH/transform_by_anchor.py"\
+    --src_filepath $HE_TIFF_FILEPATH\
+    --anchor_filepath $ANCHOR_FILEPATH\
+    --out_filepath $HE_TIFF_FILEPATH\
     --overwrite "True"
